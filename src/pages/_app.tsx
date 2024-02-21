dd83de9aba53ac3c487c80782aa4c94008f0d70a
Samarth Saxena <samarthsaxena1672003@gmail.com>
2024-02-21 14:09:12 +0530

    done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+'use client'
+import { Provider } from "@self.id/react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }:AppProps) {
+  return (
+    <Provider >
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+export default MyApp;
